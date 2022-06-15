07e6b077d66bc9c68beaef9c27585feb2ccf224e
PoojaBanjan24 <100478049+PoojaBanjan24@users.noreply.github.com>
2022-06-15 01:31:11 -1200

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <div class="center">
       <ul>
         <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">View All Users</button></a></li>
-        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Trasnfer Money</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Transfer Money</button></a></li>
         <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">View Transfer History</button></a></li>
         <li class="operations"> </li>
       </ul>
